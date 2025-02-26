33cc734bc4f1c7576cbf83b9ccbec686fbac4d93
Ajay Chauhan <42740900+akchauhan2@users.noreply.github.com>
2025-02-26 17:05:56 +0530

    Update webpack.config.js
    Added webpack bundle file names that earlier had conflict

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
